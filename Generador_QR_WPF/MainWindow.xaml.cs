a1f964471f17f8c4f45fe00c3e1389af4f868748
Ruben Rossnagel <rubenrossnagel@gmail.com>
2023-03-24 23:21:04 -0300

    Primer commit

---
+++ Generador_QR_WPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Generador_QR_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txt.Focus();
+        }
+        private void GenerateButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            string codeValue = Convert.ToString(txt.Text);
+            var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(codeValue, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new PngByteQRCode(qrCodeData);
+            var qrCodeBytes = qrCode.GetGraphic(100);
+            var bitmapImage = new BitmapImage();
+            using (var memoryStream = new MemoryStream(qrCodeBytes))
+            {
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = memoryStream;
+                bitmapImage.EndInit();
+            }
+            QrCodeContainer.Content = new Image { Source = bitmapImage };
+        }
+    }
+}
